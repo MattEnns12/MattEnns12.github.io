971dcec2e44f27e9b18c4187b6a99415372550fa
Matthew Enns <mattenns12@gmail.com>
2023-03-08 10:47:05 -0500

    Added index how to play page

---
+++ index.js
@@ -0,0 +1,25 @@
+window.onload=function(){
+    // Red Squares Click
+    let whiteSqr = document.getElementById("white-sqr");
+    if(whiteSqr){
+        whiteSqr.addEventListener('click', function(){
+
+            let purplewhite = document.getElementById("purple-white");
+            let purplewhiteimgsrc = purplewhite.src;
+
+            let lightbluewhite = document.getElementById("lightblue-white");
+            let lightbluewhiteimgsrc = lightbluewhite.src;
+
+            let lightgreenwhite = document.getElementById("lightgreen-white");
+            let lightgreenwhiteimgsrc = lightgreenwhite.src;
+
+            let orangewhite = document.getElementById("orange-white");
+            let orangewhiteimgsrc = orangewhite.src;
+
+            purplewhite.src = orangewhiteimgsrc;
+            lightbluewhite.src = purplewhiteimgsrc;
+            lightgreenwhite.src = lightbluewhiteimgsrc;
+            orangewhite.src = lightgreenwhiteimgsrc;
+        });
+    }
+}
